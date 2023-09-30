c7b8ad63aa9f19d4bd1947c7e2fc34a0d644db22
OODemi52 <demidaniel98@gmail.com>
2023-09-30 12:38:14 -0400

    fixed errors in the empty compenents

--- frontend/src/components/ImageCard.tsx
+++ frontend/src/components/ImageCard.tsx
@@ -1,14 +1,3 @@
-import * from "react";
-import Image from "./Image";
-import Text from "./Text";
-
-
 export default function ImageCard() {
 
-    return (
-    <>
-        <Image />
-        <Text />
-    </>
-    );
 }
