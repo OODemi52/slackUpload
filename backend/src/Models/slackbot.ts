1854becbe45c06516ea1b7395356cc61a1a4b1cd
OODemi52 <demidaniel98@gmail.com>
2024-02-06 23:20:41 -0500

    Moved deletion of files to slackbot file

--- backend/src/Models/slackbot.ts
+++ backend/src/Models/slackbot.ts
@@ -1,4 +1,5 @@
 import * as dotenv from 'dotenv';
+import fs from 'fs';
 import { WebClient } from '@slack/web-api';
 
 dotenv.config();
@@ -38,25 +39,40 @@ export default class SlackBot {
     }
   }
 
+  async deletionPromises(uploadedFiles: UploadedFile[]): Promise<void[]> {
+    const deletionPromises = uploadedFiles.map((file: UploadedFile) =>
+      fs.promises.unlink(file.path).then(() => {
+        console.log(`Successfully deleted file ${file.path}`);
+      }).catch((unlinkErr) => {
+        console.error(`Error deleting file ${file.path}: ${unlinkErr}`);
+      })
+    );
+
+    return Promise.all(deletionPromises);
+  }
+
   async batchAndUploadFiles(uploadedFiles: UploadedFile[], message_length: number): Promise<void> {
     const sortedFiles = uploadedFiles.sort((a, b) => {
       return a.name.localeCompare(b.name, undefined, { numeric: true, sensitivity: 'base' });
     });
-  
-    const files_upload: { filename: string, file: string }[] = [];
-  
-    for (let i = 0; i < sortedFiles.length; i++) {
-      const filename: string = sortedFiles[i].name;
-      const file: string = sortedFiles[i].path;
-  
-      files_upload.push({ file, filename });
-      console.log('Files to upload:', files_upload);
-  
-      if ((i + 1) % message_length === 0 || i === sortedFiles.length - 1) {
-        await this.uploadFilesToSlackChannel(files_upload);
-        files_upload.length = 0;
-      }
+
+    const totalBatches = Math.ceil(sortedFiles.length / message_length);
+
+    for (let i = 0; i < sortedFiles.length; i += message_length) {
+      const batchFiles = sortedFiles.slice(i, i + message_length);
+      const files_upload = batchFiles.map(file => ({
+        filename: file.name,
+        file: file.path
+      }));
+
+      const batchNumber = Math.floor(i / message_length) + 1;
+      console.log(`Preparing batch ${batchNumber} out of ${totalBatches} for upload. Files to upload:`, files_upload);
+
+      await this.uploadFilesToSlackChannel(files_upload);
+      await this.deletionPromises(batchFiles);
     }
+
+    console.log(`All files uploaded to Slack`);
   }
   
   private async uploadFilesToSlackChannel(file_uploads: { filename: string, file: string }[]): Promise<void> {
