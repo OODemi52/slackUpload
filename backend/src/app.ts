52f49bb3fc66d35457b490a6d1b4aae308d46f07
Demilade Daniel-Akanle <76265069+OODemi52@users.noreply.github.com>
2024-09-03 14:48:01 -0400

    Update CORS configuration to use cors package

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import timeout from 'connect-timeout';
 import dbConnect from "./Config/dbConnect.config";
 import apiRouter from './Routes/api.route';
 import authRouter from './Routes/auth.route';
@@ -9,41 +11,36 @@ import healthRouter from './Routes/health.route';
 
 export const app: express.Application = express();
 
-app.use((request: express.Request, response: express.Response, next: express.NextFunction) => {
-  const allowedOrigins = ["https://www.slackshots.app", "https://slackshots.app", "http://127.0.0.1:5173", "http://127.0.0.1:5174", "http://127.0.0.1:3000", "http://127.0.0.1:3001"];
-  const origin = request.headers.origin as string;
 
-  console.log('Origin:', origin);
-  console.log('Is origin allowed:', allowedOrigins.includes(origin));
-  
-  if (allowedOrigins.includes(origin)) {
-    response.setHeader('Access-Control-Allow-Origin', origin);
-    response.setHeader('Access-Control-Allow-Credentials', 'true');
-  }
-  
-  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-  next();
-});
+// CORS
+const allowedOrigins = ["https://www.slackshots.app", "https://slackshots.app", "http://127.0.0.1:5173", "http://127.0.0.1:5174", "http://127.0.0.1:3000", "http://127.0.0.1:3001"];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.use(timeout('420s'));
+
+// Middleware
 app.use(bodyParser.json({ limit: '50mb'}));
 app.use(bodyParser.urlencoded({ limit: '50mb',extended: true }));
-
 app.use(cookieParser());
-
 app.use(helmet());
 
 // API Routes
 app.use('/api', apiRouter);
-
-// Auth Routes
 app.use('/auth', authRouter);
-
-// Health Check Routes
 app.use('/health', healthRouter);
 
-// Connect to MongoDB
 dbConnect();
 
 export default app;
