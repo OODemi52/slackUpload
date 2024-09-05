6e1bf00ad461a3b61c1e1252415ec1c9fd30ba60
Demilade Daniel-Akanle <76265069+OODemi52@users.noreply.github.com>
2024-09-05 15:13:59 -0400

    Attempt to fix upload grid appearing over top select menus

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -106,7 +106,6 @@ const Dashboard: React.FC = () => {
             bg="#282828"
             overflowY="auto"
             boxShadow="inset 0 0 8px rgba(0, 0, 0, 0.6)"
-            zIndex={10}
             minH={{ base: "65vh", md: "auto" }}
           >
            <MainContent
